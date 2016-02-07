1b83a4a390eaecac7cdf7c8a21c5f43ab8330bcc
dazeycm <craigdazey@gmail.com>
2016-02-06 22:50:06 -0500

    schedule intervals, print runs to console

--- Back/gather.js
+++ Back/gather.js
@@ -1,19 +1,21 @@
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("../speeds.db");
-
 var spawn = require('child_process').exec;
-var speeds = spawn('python c:\\Python34\\Lib\\site-packages\\speedtest_cli.py --simple', function(error, stdout, stderr) {
-  ret = stdout.split("\n");
-  data = {
-    ping: ret[0].split(" ")[1],
-    down: ret[1].split(" ")[1],
-    up: ret[2].split(" ")[1],
-    date: Date.now()
-  }
 
-  saveData(data);
-});
+setInterval(function getData() {
+  var speeds = spawn('python c:\\Python34\\Lib\\site-packages\\speedtest_cli.py --simple', function(error, stdout, stderr) {
+    ret = stdout.split("\n");
+    data = {
+      ping: ret[0].split(" ")[1],
+      down: ret[1].split(" ")[1],
+      up: ret[2].split(" ")[1],
+      date: Date.now()
+    }
 
+    saveData(data);
+    console.log("Data saved on " + new Date(data.date).toString())
+  });
+}, 600000);
 
 function saveData(data) {
   db.run("INSERT INTO speeds VALUES(?, ?, ?, ?)", data.ping, data.down, data.up, data.date);
